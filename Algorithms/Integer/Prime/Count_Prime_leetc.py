955a3b0629283b383da26b0aeb131fcfdffea10e
newpheeraphat <77052746+newpheeraphat@users.noreply.github.com>
2021-06-25 23:23:54 +0700

    Create Count_Prime_leetc.py

---
+++ Algorithms/Integer/Prime/Count_Prime_leetc.py
@@ -0,0 +1,10 @@
+count = 0
+        primes = [False for i in range(n)]
+        for i in range(2,n):
+            if primes[i] == False:
+                count+=1
+                j = 2
+            while j*i<n:
+                primes[j*i] = True
+                j+=1
+        return count
