cf7bde0178d9a0f51005d9c1f360e9b8749d35ad
newpheeraphat <77052746+newpheeraphat@users.noreply.github.com>
2021-07-06 17:04:48 +0700

    Update Index_filter.py

--- Algorithms/String/Index_filter.py
+++ Algorithms/String/Index_filter.py
@@ -1,3 +1,5 @@
 def index_filter(indexes, string):
 	res = [string[i] for i in indexes]
 	return "".join(res).lower()
+# using lambda function
+index_filter=lambda i,s:''.join(s[x].lower()for x in i)
